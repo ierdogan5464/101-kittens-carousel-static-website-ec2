48d96a5486d40ad5e41650b6a30cfab8926df09b
Ishak <ierdogan5464@gmail.com>
2024-04-03 13:12:40 +0300

    second commit

---
+++ userdata.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+dnf update -y
+dnf install httpd -y
+FOLDER="https://raw.githubusercontent.com/ierdogan5464/101-kittens-carousel-static-website-ec2/main/static-web"
+cd /var/www/html
+wget ${FOLDER}/index.html
+wget ${FOLDER}/cat0.jpg
+wget ${FOLDER}/cat1.jpg
+wget ${FOLDER}/cat2.jpg
+systemctl start httpd
+systemctl enable httpd
